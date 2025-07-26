c4493f9a6c688c0d59bfbb53171d2b62f55db237
beambeambeam <supawitmarayat@gmail.com>
2025-07-26 14:43:57 +0700

    feat: add voice

--- src/app/choice/[id]/page.tsx
+++ src/app/choice/[id]/page.tsx
@@ -4,6 +4,18 @@ import { CHOICE } from "@/app/choice/choice";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import {
+  MediaPlayer,
+  MediaPlayerAudio,
+  MediaPlayerControls,
+  MediaPlayerLoop,
+  MediaPlayerPlay,
+  MediaPlayerPlaybackSpeed,
+  MediaPlayerSeek,
+  MediaPlayerSeekBackward,
+  MediaPlayerSeekForward,
+  MediaPlayerVolume,
+} from "@/components/ui/media-player";
 
 function ChoiceId() {
   const params = useParams<{ id: string }>();
@@ -23,6 +35,22 @@ function ChoiceId() {
   return (
     <div className="h-screen w-screen flex items-center justify-center flex-col">
       {choice.label}
+
+      <MediaPlayer className="h-20 w-full">
+        <MediaPlayerAudio className="sr-only">
+          <source
+            src="https://1i39q4zekz.ufs.sh/f/zi7vM2Za6Ydh6oE7gSWT0A2GFRb3mNUqzJen5XHlxi7vLuhC"
+            type="audio/mp3"
+          />
+        </MediaPlayerAudio>
+        <MediaPlayerControls className="flex-col items-start gap-2.5">
+          <MediaPlayerSeek withTime />
+          <div className="flex w-full items-center justify-center gap-2">
+            <MediaPlayerPlay />
+            <MediaPlayerVolume />
+          </div>
+        </MediaPlayerControls>
+      </MediaPlayer>
     </div>
   );
 }
--- src/app/choice/choice.ts
+++ src/app/choice/choice.ts
@@ -1,11 +1,13 @@
 type CHOICE = {
   id: string;
   label: string;
+  image: string;
 };
 
 export const CHOICE: CHOICE[] = [
   {
     id: "1",
     label: "1",
+    image: "1",
   },
 ];
