7262b8c866fabc60e43b4275477d78ee87a7c3e1
beambeambeam <supawitmarayat@gmail.com>
2025-07-26 22:32:25 +0700

    feat: enhance layout of Choice and Home components with improved structure and spacing

--- src/app/choice/page.tsx
+++ src/app/choice/page.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 
 function Choice() {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-full h-full flex-col gap-2">
+      <h1 className="text-3xl font-bold">Choose the challenge</h1>
       {CHOICE.map((c) => (
         <Link href={`/choice/${c.id}`} key={c.id}>
           <Button>{c.label}</Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center flex-col gap-2">
       <p className="text-3xl font-bold">Thai Superman Voiceover Challenge</p>
-      <Link href="/choice"></Link>
+      <Link href="/choice">
+        <Button>Start</Button>
+      </Link>
     </div>
   );
 }
